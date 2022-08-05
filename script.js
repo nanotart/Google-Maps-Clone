0002bfc83bdc2d40aa56854b15692a4003b46fa6
nanotart <shway2020@gmail.com>
2022-08-05 12:49:57 -0700

    workable map uploaded from api:

---
+++ script.js
@@ -0,0 +1,35 @@
+// this is from the API: https://docs.mapbox.com/mapbox-gl-js/guides/install/
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibmFub3RhcnQiLCJhIjoiY2w2Z3Vld2J2MnFjZTNkcXM2bmxkcjRnNCJ9.SYvcLi4HByBw791VSeVbmA';
+mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+
+// API call geolocation to get current position
+navigator.geolocation.getCurrentPosition(successLocation, 
+    errorLocation, { 
+    enableHighAccuracy: true
+})
+
+function setupMap(centerPosition) {
+    //create a new map
+    const map = new mapboxgl.Map({
+        accessToken: MAPBOX_ACCESS_TOKEN,
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: centerPosition,
+        zoom: 15,
+        projection: 'globe'
+    });
+    map.on('style.load', () => {
+        map.setFog({})
+    });
+}
+
+// Function for a successful location
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+// Function for a nonexistant location
+function errorLocation() {
+    setupMap([-2.24, 53.48])
+}
+
